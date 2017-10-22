183e25bc6d2945cb8395935469228c1076a21ca8
Pal <palaniscit@gmail.com>
2017-10-21 22:57:22 -0400

    First Commit.

---
+++ src/main/java/com/redstrikers/predictions/PredictionsApplication.java
@@ -0,0 +1,12 @@
+package com.redstrikers.predictions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/redstrikers/predictions/PredictionsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.redstrikers.predictions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PredictionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
